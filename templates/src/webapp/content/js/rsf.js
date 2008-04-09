141353746ac108f5209468ec7a15e338c38a138a
Steven Githens <swgithen@mtu.edu>
2008-04-09 17:49:32 +0000

    NOJIRA Added awesome fireUVBrequest function that you can use to directly use UVB from js.

--- templates/src/webapp/content/js/rsf.js
+++ templates/src/webapp/content/js/rsf.js
@@ -1133,6 +1133,50 @@ var RSF = function() {
       
       return updatedElements;
     }
+    
+    /**
+     * This is the leek and potatos RSF UVB function I've always wanted,
+     * designed for the UVB enthusiast who knows what they want. It takes
+     * the UVB URL, the bindings/values to send over, the bindings you 
+     * want back, and an optional action binding.
+     *
+     * @token A special token depicting this request. You can often put in whatever
+     * you like, but this can be used to stop the dreaded double submission effects.
+     * If a post with the same token is already being processed, subsequent ones
+     * will be ignored.
+     *
+     * @uvburl The url. In practice this usually looks something like,
+     *   http://server/myapp/faces/UVBview, though it's best to generate it with
+     *   viewStateHandler.getFullURL(new SimpleViewParameters(UVBProducer.VIEW_ID))
+     *
+     * @inbindings This should be a standard object hash of bindings to values.
+     *   ex. inbindings['mybean.value1'] = 'one';  
+     *       inbindings['mybean.value2'] = 'two';
+     *
+     * @outbindings This should be an array of bindings you want back from the request.
+     *   ex. outbindings[0] = 'mybean.value3'
+     *       outbindings[1] = 'mybean.value4'
+     *
+     * @actionbinding This should a String with the actionbinding. Can be null if
+     *   you don't want one.  ex. 'mybean.execute'
+     *
+     * @callback This should be a standard javascript object containing the usual
+     * callback functions such as success.
+     */
+    function fireUVBRequest(token, uvburl, inbindings, outbindings, actionbinding, callback) {
+      var queries = new Array();
+      for (var i in inbindings) {
+        queries.push(RSF.renderBinding(i,inbindings[i]));
+      }
+      if (actionbinding != null) {
+        queries.push(RSF.renderActionBinding(actionbinding));
+      }
+      for (var i in outbindings) {
+        queries.push(RSF.renderUVBQuery(outbindings[i]));
+      }
+      var body = queries.join("&");
+      RSF.queueAJAXRequest(token,"POST",uvburl,body,callback);
+    }
 
   }; // end return internal "Object"
 }(); // end namespace RSF
